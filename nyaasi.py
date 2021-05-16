6e4458a7bde7d9ed795190ff0c973e55fb66e252
kamileecher <58888200+kamileecher@users.noreply.github.com>
2021-05-16 23:12:44 +0300

    Update nyaasi.py

--- nyaasi.py
+++ nyaasi.py
@@ -8,18 +8,18 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 app = Client("trntsrcbot", api_id=int(os.environ.get("API_ID")), api_hash=os.environ.get("API_HASH"), bot_token=os.environ.get("BOT_TOKEN"))
 
 
-print("\nBot Started\n")
+print("\nMerhaba\n")
 
 
 @app.on_message(filters.command(['start']))
 async def start(_, message):
-    await message.reply_text("Hello I'm NyaaSi Torrent Scraper Bot\nSend /help To Show Help Screen\nBot by @unkusr")
+    await message.reply_text("yardım için /help komutunu tıkla")
 
 
 
 @app.on_message(filters.command(['help']))
 async def help(_, message):
-    await message.reply_text("Example: /find titanic")
+    await message.reply_text("Örnek: /find naruto")
 
 m = None
 i = 0
@@ -38,35 +38,35 @@ async def find(_, message):
     except:
         pass
     if len(message.command) < 2:
-        await message.reply_text("Usage: /find query")
+        await message.reply_text("Kullanım: /find naruto")
         return
     query = message.text.split(None, 1)[1].replace(" ", "%20")
-    m = await message.reply_text("Searching")
+    m = await message.reply_text("Aranıyor")
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(f"https://api.api-zero.workers.dev/nyaasi/{query}") \
                     as resp:
                 a = json.loads(await resp.text())
     except:
-        await m.edit("Found Nothing.")
+        await m.edit("🥺Hiçbir şey bulunamadı..")
         return
     result = (
-        f"**Page - {i+1}**\n\n"
-        f"➲Name: {a[i]['Name']}\n"
-        f"➲Uploaded on {a[i]['Date']}\n"
-        f"➲Torrent: {a[i]['TorrentLink']}\n" 
-        f"➲Type: {a[i]['Category']}\n"
-        f"➲Size: {a[i]['Size']}\n"
-        f"➲Seeds: {a[i]['Seeder']} & "
-        f"➲Leeches: {a[i]['Leecher']}\n"
-        f"➲Magnet: `{a[i]['Magnet']}`\n\n\n"
+        f"**📑Page - {i+1}**\n\n"
+        f"✔️Name: {a[i]['Name']}\n"
+        f"✔️Uploaded on {a[i]['Date']}\n"
+        f"✔️Torrent: {a[i]['TorrentLink']}\n" 
+        f"✔️Type: {a[i]['Category']}\n"
+        f"✔️Size: {a[i]['Size']}\n"
+        f"✔️Seeds: {a[i]['Seeder']} & "
+        f"✔️Leeches: {a[i]['Leecher']}\n"
+        f"✔️Magnet: `{a[i]['Magnet']}`\n\n\n"
     )
     await m.edit(
         result,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(f"Next",
+                    InlineKeyboardButton(f"İleri",
                                          callback_data="next"),
                     InlineKeyboardButton(f"{emoji.CROSS_MARK}",
                                          callback_data="delete")
@@ -85,26 +85,26 @@ async def callback_query_next(_, message):
     global query
     i += 1
     result = (
-        f"**Page - {i+1}**\n\n"
-        f"➲Name: {a[i]['Name']}\n"
-        f"➲Uploaded on {a[i]['Date']}\n"
-        f"➲Torrent: {a[i]['TorrentLink']}\n" 
-        f"➲Type: {a[i]['Category']}\n"
-        f"➲Size: {a[i]['Size']}\n"
-        f"➲Seeds: {a[i]['Seeder']} & "
-        f"➲Leeches: {a[i]['Leecher']}\n"
-        f"➲Magnet: `{a[i]['Magnet']}`\n\n\n"
+        f"**📑Page - {i+1}**\n\n"
+        f"✔️Name: {a[i]['Name']}\n"
+        f"✔️Uploaded on {a[i]['Date']}\n"
+        f"✔️Torrent: {a[i]['TorrentLink']}\n" 
+        f"✔️Type: {a[i]['Category']}\n"
+        f"✔️Size: {a[i]['Size']}\n"
+        f"✔️Seeds: {a[i]['Seeder']} & "
+        f"✔️Leeches: {a[i]['Leecher']}\n"
+        f"✔️Magnet: `{a[i]['Magnet']}`\n\n\n"
     )
     await m.edit(
         result,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(f"Prev",
+                    InlineKeyboardButton(f"Geri",
                                          callback_data="previous"),
                     InlineKeyboardButton(f"{emoji.CROSS_MARK}",
                                          callback_data="delete"),
-                    InlineKeyboardButton(f"Next",
+                    InlineKeyboardButton(f"İleri",
                                          callback_data="next")
                     
                 ]
@@ -122,26 +122,26 @@ async def callback_query_previous(_, message):
     global query
     i -= 1
     result = (
-        f"**Page - {i+1}**\n\n"
-        f"➲Name: {a[i]['Name']}\n"
-        f"➲Uploaded on {a[i]['Date']}\n"
-        f"➲Torrent: {a[i]['TorrentLink']}\n" 
-        f"➲Type: {a[i]['Category']}\n"
-        f"➲Size: {a[i]['Size']}\n"
-        f"➲Seeds: {a[i]['Seeder']} & "
-        f"➲Leeches: {a[i]['Leecher']}\n"
-        f"➲Magnet: `{a[i]['Magnet']}`\n\n\n"
+        f"**📑Page - {i+1}**\n\n"
+        f"✔️Name: {a[i]['Name']}\n"
+        f"✔️Uploaded on {a[i]['Date']}\n"
+        f"✔️Torrent: {a[i]['TorrentLink']}\n" 
+        f"✔️Type: {a[i]['Category']}\n"
+        f"✔️Size: {a[i]['Size']}\n"
+        f"✔️Seeds: {a[i]['Seeder']} & "
+        f"✔️Leeches: {a[i]['Leecher']}\n"
+        f"✔️Magnet: `{a[i]['Magnet']}`\n\n\n"
     )
     await m.edit(
         result,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(f"Prev",
+                    InlineKeyboardButton(f"Geri",
                                          callback_data="previous"),
                     InlineKeyboardButton(f"{emoji.CROSS_MARK}",
                                          callback_data="delete"),
-                    InlineKeyboardButton(f"Next",
+                    InlineKeyboardButton(f"İleri",
                                          callback_data="next")
                 ]
             ]
